c9c865fa406b482b9e13a699c22579e7760297c4
machen18 <machen18@s.sfusd.edu>
2023-12-17 18:31:59 -0800

    updated world scene setup

--- Player.gd
+++ Player.gd
@@ -9,7 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move right"):
+	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
